49347c78a51c2666710567a6ae539358b63d07ad
flarrea <flarreas@gmail.com>
2020-06-01 19:37:45 -0400

    #02 Ecomapp Conectar con MySQL y desplegar productos

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pymysql
+
+app = Flask(__name__)
+
+def get_connection():
+    return pymysql.connect(host='******gear.host',
+                             user='*****omapp',
+                             password='*******',
+                             db='*******',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 app = Flask(__name__)
 
@@ -9,6 +20,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/productos1')
+def productos1():
+    connection = get_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM productos"
+            cursor.execute(sql)
+            productos = cursor.fetchall()
+            productos = list(productos)
+    finally:
+            connection.close()
+
+    return render_template('productos1.html', productos = productos)
 
 if __name__ == '__main__':
         app.run(debug=True)
